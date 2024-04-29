358ea73bfac4deaa734fe2cb66610cd59af2e1d1
Anna <kupriienko16@gmail.com>
2024-04-29 11:11:36 +0100

    1

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,10 +1,10 @@
-
+import css from "./Info.module.css"
 
 export default function Info({ movies }) {
     const defaultImg =
         '<https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg>'
     return (
-        <>
+        <div className={css.container}>
         <img src={
  movies.poster_path ? `https://image.tmdb.org/t/p/w500/${movies.poster_path}` 
  : defaultImg}
@@ -13,16 +13,16 @@ alt="poster"
             />
             <div>
                 <h1>{movies.title}</h1>
-                <p>Use Score: { movies.average}</p>
+                <p>Use Score: { movies.vote_average} %</p>
                 <h2>Overview</h2>
                 <p>{movies.overview}</p>
                 <h2>Genres</h2>
                 <ul>
-                    {movies.map((movie) => {
+                    {movies.genres.map((movie) => {
                         return(<li key={movie.id}> {movie.name}</li>
                     )})}
-                </ul>
+                </ul> 
             </div>
-</>
+</div>
     );
 }
